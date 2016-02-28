b5e42372d694dde858ba66322ed8892c9966e86e
mr.Shu <mr@shu.io>
2016-02-28 16:59:13 +0100

    update: Templatize runner
    * Templatize runner so that it can be reused for many other challenges.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ challenger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GTA_BIN=${GTA_BIN=:"$CURRENT_DIR/go-term-adventure"}
+if [ ! -e $CHALLENGE_FILE ];
+then
+    echo "Challenge file $CHALLENGE_FILE does not exist."
+    exit 1;
+fi
+CHALLENGE_NAME="$(basename $CHALLENGE_FILE | sed 's/\..*$//')"
+
+PROMPT_COMMAND="$GTA_BIN $CHALLENGE_FILE" HISTFILE=$HOME/.gtahistory bash
+rm -rf $HOME/.gtahistory
+rm -rf $HOME/.config/$CHALLENGE_NAME

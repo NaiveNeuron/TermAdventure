fcbbeb40326dab0cbd6aeb4fc4383514da1d9853
mr.Shu <mr@shu.io>
2017-03-25 21:33:09 +0100

    update: Further challenger and bashrc updates
    * Add some more updates to challenger and bashrc
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- challenger.sh
+++ challenger.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GTA_BIN=${GTA_BIN=:"$CURRENT_DIR/go-term-adventure"}
+GTA_BIN=${GTA_BIN=:"$CURRENT_DIR/go-term-adventure.64"}
 if [ ! -e $CHALLENGE_FILE ];
 then
     echo "Challenge file $CHALLENGE_FILE does not exist."
     exit 1;
 fi
-CHALLENGE_NAME="$(basename $CHALLENGE_FILE | sed 's/\..*$//')"
+CHALLENGE_NAME=${CHALLENGE_NAME=:"$(basename $CHALLENGE_FILE | sed 's/\.[^\.]*$//')"}
 
-PROMPT_COMMAND="history -a; $GTA_BIN $CHALLENGE_FILE"\
-        bash --rcfile $CURRENT_DIR/gta_bashrc | tee $HOME/.gtaoutput
+# Prepare global ENV variables for child processes
+export GTA_BIN
+export CHALLENGE_FILE
 
+# Run bash
+bash --rcfile $CURRENT_DIR/gta_bashrc
+
+# Clean up used files
 rm -rf $HOME/.gtahistory
 rm -rf $HOME/.config/$CHALLENGE_NAME
+
+# Unset global ENV variables
+unset GTA_BIN
+unset CHALLENGE_FILE
